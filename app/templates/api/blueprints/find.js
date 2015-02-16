4c24e88929a22e93b55c9b13997a2501823dcf4d
Eugene Obrezkov <ghaiklor@gmail.com>
2015-02-16 14:00:01 +0200

    Resolve #32

--- app/templates/api/blueprints/find.js
+++ app/templates/api/blueprints/find.js
@@ -12,16 +12,31 @@ module.exports = function (req, res) {
     if (actionUtil.parsePk(req)) return require('./findOne')(req, res);
 
     var Model = actionUtil.parseModel(req),
-        query = Model
-            .find()
-            .where(actionUtil.parseCriteria(req))
-            .limit(actionUtil.parseLimit(req))
-            .skip(actionUtil.parseSkip(req))
-            .sort(actionUtil.parseSort(req));
+        where = actionUtil.parseCriteria(req),
+        limit = actionUtil.parseLimit(req),
+        skip = actionUtil.parseSkip(req),
+        sort = actionUtil.parseSort(req),
+        query = Model.find().where(where).limit(limit).skip(skip).sort(sort);
 
     query = actionUtil.populateEach(query, req);
     query.exec(function (error, records) {
         if (error) return res.serverError(error);
-        return res.ok(records);
+
+        Model
+            .count()
+            .exec(function (error, count) {
+                if (error) return res.serverError(error);
+
+                var metaInfo = {
+                    start: skip,
+                    end: skip + limit,
+                    limit: limit,
+                    total: count,
+                    criteria: where
+                };
+
+                res.set('Content-Range', metaInfo.start + '-' + metaInfo.end + '/' + metaInfo.total);
+                return res.ok(records, null, null, metaInfo);
+            });
     });
 };
