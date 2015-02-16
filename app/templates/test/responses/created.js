c6010e3754054a70d68cfe5a37b2f4e842c86081
Eugene Obrezkov <ghaiklor@gmail.com>
2015-02-16 15:38:09 +0200

    Add blank test for rest of responses

---
+++ app/templates/test/responses/created.js
@@ -0,0 +1,8 @@
+var assert = require('assert'),
+    created = require('../../api/responses/created');
+
+describe("responses:created", function () {
+    it("TODO: write this test", function () {
+        assert(true);
+    });
+});
---
+++ app/templates/test/responses/unauthorized.js
@@ -0,0 +1,8 @@
+var assert = require('assert'),
+    unauthorized = require('../../api/responses/unauthorized');
+
+describe("responses:unauthorized", function () {
+    it("TODO: write this test", function () {
+        assert(true);
+    });
+});
