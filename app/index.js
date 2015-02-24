6b32dff37bf5d40b7ad7b173b7a214cf54faca34
Eugene Obrezkov <ghaiklor@gmail.com>
2015-02-24 15:40:49 +0200

    Add separators with notification to questions

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ var crypto = require('crypto'),
     printMessage = require('print-message'),
     updateNotifier = require('update-notifier'),
     yeoman = require('yeoman-generator'),
-    yosay = require('yosay');
+    yosay = require('yosay'),
+    inquirer = require('yo/node_modules/inquirer');
 
 /**
  * List of questions
@@ -80,61 +81,88 @@ var QUESTIONS_LIST = [{
     type: "checkbox",
     name: "services:cipher",
     message: "Choose which Cipher services you want",
-    default: ["bcrypt", "jwt"],
-    choices: [
-        "bcrypt",
-        "jwt"
-    ]
+    choices: [{
+        name: "bcrypt",
+        checked: true
+    }, {
+        name: "jwt",
+        checked: true
+    }, new inquirer.Separator(), {
+        name: "More Cipher is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:mailer",
     message: "Choose which Mailer services you want",
-    default: [],
-    choices: [
-        "Mandrill"
-    ]
+    choices: [{
+        name: "Mandrill",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More Mailer is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:payment",
     message: "Choose which Payment services you want",
-    default: [],
-    choices: [
-        "Stripe"
-    ]
+    choices: [{
+        name: "Stripe",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More Payment is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:pusher",
     message: "Choose which Pusher services you want",
-    default: [],
-    choices: [
-        "Apple Push Notification",
-        "Google Cloud Messaging"
-    ]
+    choices: [{
+        name: "Apple Push Notification",
+        checked: false
+    }, {
+        name: "Google Cloud Messaging",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More Pusher is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:sms",
     message: "Choose which SMS services you want",
-    default: [],
-    choices: [
-        "Twilio"
-    ]
+    choices: [{
+        name: "Twilio",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More SMS is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:social",
     message: "Choose which Social services you want",
-    default: [],
-    choices: [
-        "Facebook"
-    ]
+    choices: [{
+        name: "Facebook",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More Social is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "checkbox",
     name: "services:storage",
     message: "Choose which Storage services you want",
-    default: [],
-    choices: [
-        "Amazon S3",
-        "Google Cloud Storage"
-    ]
+    choices: [{
+        name: "Amazon S3",
+        checked: false
+    }, {
+        name: "Google Cloud Storage",
+        checked: false
+    }, new inquirer.Separator(), {
+        name: "More Storage is coming",
+        disabled: "Wait for it..."
+    }]
 }, {
     type: "confirm",
     name: "docs:include",
