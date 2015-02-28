86b0648130ddd41689709081ce81af99bae09737
Eugene Obrezkov <ghaiklor@gmail.com>
2015-02-28 18:24:27 +0200

    Add comment how prompting is working

--- generators/app/steps/prompting.js
+++ generators/app/steps/prompting.js
@@ -1,6 +1,9 @@
 /**
  * Step 2
- * Where you prompt users for options (where you'd call this.prompt())
+ * Where you prompt users for options (where you'd call this.prompt()).
+ *
+ * Questions is required from ../questions folder.
+ * Each of answers is mixin to this.answers attribute and available in templates as answers variable.
  */
 
 var chalk = require('chalk'),
