90064f25427f49c9223e9b7a0911d4f2722001f8
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-13 19:34:19 +0200

    Add disk connection by default in development env

--- generators/app/templates/config/env/development.js
+++ generators/app/templates/config/env/development.js
@@ -3,4 +3,8 @@
  * @description :: This section overrides all other config values ONLY in development environment
  */
 
-module.exports = {};
+module.exports = {
+    models: {
+        connection: 'disk'
+    }
+};
