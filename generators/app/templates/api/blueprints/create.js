93f3eb2b3c79d6635fffce29983e714256c4b392
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-31 12:57:26 +0300

    Move model create to private fn

--- generators/app/templates/api/blueprints/create.js
+++ generators/app/templates/api/blueprints/create.js
@@ -1,6 +1,21 @@
 var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
 
 /**
+ * Triggers when Model is created
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Object} error
+ * @param {Object} record
+ * @returns {*}
+ * @private
+ */
+function _onModelCreate(req, res, error, record) {
+  if (error) return res.serverError(error);
+
+  return res.created(record);
+}
+
+/**
  * Create Record
  * POST /:model
  *
@@ -10,11 +25,5 @@ module.exports = function (req, res) {
   var Model = actionUtil.parseModel(req);
   var data = actionUtil.parseValues(req);
 
-  Model
-    .create(data)
-    .exec(function (error, record) {
-      if (error) return res.serverError(error);
-
-      return res.created(record);
-    });
+  Model.create(data).exec(_onModelCreate.bind(req, res));
 };
