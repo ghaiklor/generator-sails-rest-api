a39891ad34c25b8a22e1ec05722a27235d79ea78
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-31 17:16:14 +0300

    Optimize update blueprint

--- generators/app/templates/api/blueprints/update.js
+++ generators/app/templates/api/blueprints/update.js
@@ -8,32 +8,15 @@ var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
  */
 module.exports = function (req, res) {
   var Model = actionUtil.parseModel(req);
-  var pk = actionUtil.requirePk(req);
   var values = actionUtil.parseValues(req);
 
   if (!((req.body && req.body.id) || req.query.id)) delete values.id;
 
   Model
-    .findOne(pk)
-    .exec(function (error, record) {
-      if (error) return res.serverError(error);
-      if (!record) return res.notFound();
-
-      Model
-        .update(pk, values)
-        .exec(function (error, records) {
-          if (error) return res.serverError(error);
-
-          var updatedRecord = records[0];
-          var query = Model.findOne(updatedRecord[Model.primaryKey]);
-
-          query = actionUtil.populateEach(query, req);
-          query.exec(function (error, populatedRecord) {
-            if (error) return res.serverError(error);
-            if (!populatedRecord) return res.serverError(null, null, "Could not find record after updating");
-
-            return res.ok(populatedRecord);
-          });
-        });
-    });
+    .update(actionUtil.requirePk(req), values)
+    .then(function (records) {
+      return actionUtil.populateEach(Model.findOne(records[0][Model.primaryKey]), req);
+    })
+    .then(res.ok)
+    .catch(res.serverError);
 };
