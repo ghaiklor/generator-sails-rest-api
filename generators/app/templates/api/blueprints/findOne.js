24082b26282d9dfeb7f44f5b580fa9cd2cf1fc1a
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-31 16:51:14 +0300

    Optimize findOne blueprint

--- generators/app/templates/api/blueprints/findOne.js
+++ generators/app/templates/api/blueprints/findOne.js
@@ -8,15 +8,7 @@ var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
  */
 
 module.exports = function (req, res) {
-  var Model = actionUtil.parseModel(req);
-  var pk = actionUtil.requirePk(req);
-  var query = Model.findOne(pk);
-
-  query = actionUtil.populateEach(query, req);
-  query.exec(function (error, record) {
-    if (error) return res.serverError(error);
-    if (!record) return res.notFound();
-
-    return res.ok(record);
-  });
+  actionUtil.populateEach(actionUtil.parseModel(req).findOne(actionUtil.requirePk(req)), req)
+    .then(res.ok)
+    .catch(res.serverError);
 };
