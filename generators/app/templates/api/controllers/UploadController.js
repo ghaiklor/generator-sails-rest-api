7d6de2453dfac1c3c38ccf56d9ead71535382d83
Eugene Obrezkov <ghaiklor@gmail.com>
2015-04-02 15:29:18 +0300

    Add upload controller

---
+++ generators/app/templates/api/controllers/UploadController.js
@@ -0,0 +1,16 @@
+/**
+ * UploadController
+ * @description :: Server-side logic for uploading binary files
+ */
+
+module.exports = {
+  /**
+   * Upload any file to storage and get link to it
+   * @param {Object} req Request object
+   * @param {Object} res Response object
+   */
+  index: function (req, res) {
+    // TODO: implement uploading binary files to storage
+    res.badRequest(null, null, 'Not implemented yet');
+  }
+};
