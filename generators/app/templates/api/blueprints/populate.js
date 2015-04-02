b45ce26f8c4ef5a11f551a5e34ca738796f7186d
Eugene Obrezkov <ghaiklor@gmail.com>
2015-04-02 16:44:22 +0300

    Add comments to default blueprints

--- generators/app/templates/api/blueprints/populate.js
+++ generators/app/templates/api/blueprints/populate.js
@@ -1,1 +1,8 @@
+/**
+ * Populate an association
+ * GET /:model/:parentId/:relation
+ * GET /:model/:parentId/:relation/:id
+ *
+ * Expand response with populated data from relations in models.
+ */
 module.exports = require('sails/lib/hooks/blueprints/actions/populate');
