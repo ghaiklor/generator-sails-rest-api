6397ef7daf8a078a2f4a30aadb06128f9c841bcd
Eugene Obrezkov <ghaiklor@gmail.com>
2015-04-03 15:54:30 +0300

    Remove running diagnostic from end step

--- generators/app/steps/end.js
+++ generators/app/steps/end.js
@@ -6,26 +6,7 @@
 var chalk = require('chalk');
 var printMessage = require('print-message');
 
-/**
- * Triggers when check-deps tools is finished
- * @param {Function} done
- * @private
- */
-function _onCheckDepsClose(done) {
-  this.spawnCommand('npm', ['run', 'check-updates', this.options.verbose ? '--verbose' : '']).on('close', done);
-}
-
 module.exports = {
-  /**
-   * Run diagnostic tools
-   */
-  runDiagnostic: function () {
-    if (!(this.options['skip-project-diagnostic'] || this.options['skip-all'])) {
-      this.log(chalk.yellow('Starting diagnostic, please wait...'));
-      this.spawnCommand('npm', ['run', 'check-deps', this.options.verbose ? '--verbose' : '']).on('close', _onCheckDepsClose.bind(this, this.async()));
-    }
-  },
-
   /**
    * Say warning that this generator is under development
    */
