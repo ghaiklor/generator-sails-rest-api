1498cdb25a30c035cf1e38c3702c4c95dfff882d
Eugene Obrezkov <ghaiklor@gmail.com>
2015-04-06 19:33:46 +0300

    Move check-deps tool to end step in generator core

--- generators/app/steps/end.js
+++ generators/app/steps/end.js
@@ -3,10 +3,36 @@
  * Called last, cleanup, say good bye, etc
  */
 
+var spawn = require('child_process').spawn;
 var chalk = require('chalk');
 var printMessage = require('print-message');
+var checkDependencies = require('dependency-check');
+var recursive = require('recursive-readdir');
 
 module.exports = {
+  /**
+   * Parse AST and install missing dependencies
+   */
+  installProjectDeps: function () {
+    var done = this.async();
+
+    recursive('./', ['node_modules'], function (error, files) {
+      files = files.filter(function (file) {
+        return file.split('.').pop() === 'js';
+      });
+
+      checkDependencies({
+        path: this.destinationPath('package.json'),
+        entries: files
+      }, function (error, data) {
+        var npmInstall = spawn('npm', ['install', '--save', '--color', 'always'].concat(checkDependencies.missing(data.package, data.used)));
+        npmInstall.stdout.pipe(process.stdout);
+        npmInstall.stderr.pipe(process.stderr);
+        npmInstall.on('close', done);
+      }.bind(this));
+    }.bind(this));
+  },
+
   /**
    * Say warning that this generator is under development
    */
