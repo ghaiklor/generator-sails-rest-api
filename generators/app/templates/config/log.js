3ad9bcc7c0c1aa66908aa1545fd6e253b93d81ea
Eugene Obrezkov <ghaiklor@gmail.com>
2015-04-17 18:01:42 +0300

    Add todo to daily rotate

--- generators/app/templates/config/log.js
+++ generators/app/templates/config/log.js
@@ -72,6 +72,7 @@ module.exports.log = {
    * @description Pipes logs to files sorted by dates
    * @type {Object}
    */
+  // TODO: make more beautiful log info in files, not JSON objects
   dailyRotate: {
     level: 'silly',
     timestamp: true,
