5b49833ea381bc03afa86caff10db69df45bb096
IncoCode <akulich.dimon@gmail.com>
2015-07-16 10:44:21 +0300

    Added test for UserController (ref ghaiklor/generator-sails-rest-api#20, ref ghaiklor/generator-sails-rest-api#51)

--- generators/app/templates/test/unit/controllers/UserController.test.js
+++ generators/app/templates/test/unit/controllers/UserController.test.js
@@ -1,8 +1,51 @@
 var assert = require('assert');
 var UserController = require('../../../api/controllers/UserController');
+var Promise = require('bluebird');
+
+var Users = require('../../fixtures/Users.json');
 
 describe("controllers:UserController", function () {
-  it("TODO: write this test", function () {
-    assert(true);
+  it("should return maximum 5 recently registered users", function (done) {
+    var lastUsers = Users.slice(-5); // get the last 5 users from our fixture
+
+    new Promise(function (resolve, reject) {
+      sails.requestForTest('post', '/v1/Auth/signin')
+        .send(lastUsers[0])
+        .expect(200)
+        .end(function (err, data) {
+          if (err) return reject(err);
+
+          return resolve(data.body);
+        });
+    })
+      .then(function (answer) {
+        if (!answer.data || !answer.data.token) return Promise.reject("Problem with authorizations!");
+
+        return Promise.resolve(answer.data.token);
+      })
+      .then(function (authToken) {
+        return new Promise(function (resolve, reject) {
+          sails.requestForTest('get', '/v1/User/recently_registered')
+            .set('Authorization', 'Bearer ' + authToken)
+            .expect(200)
+            .end(function (err, data) {
+              if (err) return reject(err);
+
+              return resolve(data.body);
+            });
+        })
+      })
+      .then(function (recRegistered) {
+        recRegistered.data.forEach(function (recUser) {
+          var res = lastUsers.some(function (user) {
+            return user.username === recUser.username;
+          });
+
+          assert.ok(res);
+        })
+      })
+      .then(done)
+      .catch(done)
   });
+
 });
