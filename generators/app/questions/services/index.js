eb5f656396474ff20a37a568a27bae006bd4971b
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-10 19:32:04 +0300

    Move services questions to split files

---
+++ generators/app/questions/services/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  cipher: require('./cipher'),
+  hash: require('./hash'),
+  image: require('./image'),
+  location: require('./location'),
+  mailer: require('./mailer'),
+  payment: require('./payment'),
+  pusher: require('./pusher'),
+  sms: require('./sms'),
+  social: require('./social'),
+  storage: require('./storage')
+};
