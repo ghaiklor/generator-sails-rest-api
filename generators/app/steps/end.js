cd9f5879b587f08c228043e9517747c04eea67b6
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-11 14:12:19 +0300

    Simplify end step

--- generators/app/steps/end.js
+++ generators/app/steps/end.js
@@ -5,8 +5,6 @@
 
 var path = require('path');
 var spawn = require('child_process').spawn;
-var chalk = require('chalk');
-var printMessage = require('print-message');
 var checkDependencies = require('dependency-check');
 var recursive = require('recursive-readdir');
 
@@ -14,34 +12,25 @@ module.exports = {
   /**
    * Parse AST and install missing dependencies
    */
-  installProjectDeps: function () {
+  installDependencies: function () {
     if (!this.options['skip-install']) {
       var done = this.async();
 
-      // TODO: make more simple without deps
-      // FIXME: test folder is also scanning for require and this cause issue with relative
-      recursive(this.destinationPath(), ['node_modules', 'test'], function (error, _files) {
+      recursive(this.destinationPath(), ['node_modules'], function (error, _files) {
         var files = _files.filter(function (file) {
           return file.split('.').pop() === 'js';
         }).map(function (file) {
           return path.relative(process.cwd(), file);
         });
 
-        if (this.options.verbose) {
-          this.log(chalk.yellow('Found this js files in your project:'));
-          this.log(files);
-        }
-
         checkDependencies({
           path: this.destinationPath('package.json'),
           entries: files
         }, function (error, data) {
-          var selectedAdapter = this.answers['database:adapter'].toLowerCase();
-          var adapters = ['sails-' + selectedAdapter];
-          if (selectedAdapter !== 'disk')
-            adapters.push('sails-disk');
+          var adapters = ['sails-' + this.answers['database:adapter'].toLowerCase(), 'sails-disk'];
+          var missingDependencies = checkDependencies.missing(data.package, data.used).concat(adapters);
+          var npmInstall = spawn('npm', ['install', '--save', '--color', 'always'].concat(missingDependencies));
 
-          var npmInstall = spawn('npm', ['install', '--save', '--color', 'always'].concat(checkDependencies.missing(data.package, data.used)).concat(adapters));
           npmInstall.stdout.pipe(process.stdout);
           npmInstall.stderr.pipe(process.stderr);
           npmInstall.on('close', done);
