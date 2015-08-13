4859dce8d3c702bc17e2a586d928b1dfe9efe647
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-13 15:30:55 +0300

    Remove unused policy

--- generators/app/templates/api/policies/isAllowed.js
+++
@@ -1,9 +0,0 @@
-/**
- * isAllowed
- * @description :: Policy to check if this request goes from our applications
- */
-
-module.exports = function (req, res, next) {
-  // TODO: make here more complex security
-  next();
-};
--- generators/app/templates/test/unit/policies/isAllowed.test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var isAllowed = require('../../../api/policies/isAllowed');
-
-describe("policies:isAllowed", function () {
-  it("TODO: write this test", function () {
-    assert(true);
-  });
-});
