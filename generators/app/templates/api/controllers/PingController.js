21ac3fb30092d9cb57cd8af5f7a7f09296fa2d04
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-13 15:28:23 +0300

    Typo fix in PingController

--- generators/app/templates/api/controllers/PingController.js
+++ generators/app/templates/api/controllers/PingController.js
@@ -6,8 +6,6 @@
 module.exports = {
   /**
    * Useful when need to check if it's server is down or it some logic is broken
-   * @param {Object} req Request object
-   * @param {Object} res Response object
    */
   index: function (req, res) {
     res.ok(null, null, 'HTTP server is working');
