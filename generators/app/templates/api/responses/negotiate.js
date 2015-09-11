c98e90c159ebd6a25c7aea9d1b97abc7b795e322
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-11 17:10:32 +0300

    Add passport support to negotiate response

--- generators/app/templates/api/responses/negotiate.js
+++ generators/app/templates/api/responses/negotiate.js
@@ -10,8 +10,8 @@ module.exports = function (error) {
   var res = this.res;
   var code = _.get(error, 'code');
   var message = _.get(error, 'reason') || _.get(error, 'message');
-  var data = _.get(error, 'invalidAttributes') || _.omit(error, ['code', 'reason', 'message', 'status']);
-  var statusCode = _.get(error, 'status', 500);
+  var data = _.get(error, 'invalidAttributes') || _.omit(error, ['name', 'code', 'reason', 'message', 'status', 'oauthError']);
+  var statusCode = _.get(error, 'status') || _.get(error, 'oauthError') || 500;
 
   if (statusCode === 401) return res.unauthorized(data, {code: code, message: message});
   if (statusCode === 403) return res.forbidden(data, {code: code, message: message});
