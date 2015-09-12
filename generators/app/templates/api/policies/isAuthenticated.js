0458137ee581e7b9ae4b63496c760fd964099271
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-12 09:40:42 +0300

    Fix policy for new responses

--- generators/app/templates/api/policies/isAuthenticated.js
+++ generators/app/templates/api/policies/isAuthenticated.js
@@ -3,12 +3,12 @@
  * @description :: Policy that inject user in `req` via JSON Web Token
  */
 
+var _ = require('lodash');
 var passport = require('passport');
 
 module.exports = function (req, res, next) {
   passport.authenticate('jwt', function (error, user, info) {
-    if (error) return res.serverError(error);
-    if (!user) return res.unauthorized(null, info && info.code, info && info.message);
+    if (error || !user) return res.negotiate(_.assign(error || {}, info));
 
     req.user = user;
 
