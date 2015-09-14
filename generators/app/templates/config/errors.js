1f2386b2d5b35fbf3eae3e97dbe7ee302210262b
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-14 14:51:04 +0300

    Add config file with errors

---
+++ generators/app/templates/config/errors.js
@@ -0,0 +1,13 @@
+module.exports.errors = {
+  USER_NOT_FOUND: {
+    status: 401,
+    code: 'E_USER_NOT_FOUND',
+    message: 'User with specified credentials is not found'
+  },
+
+  AUTH_FAILED: {
+    status: 401,
+    code: 'E_AUTH_FAILED',
+    message: ''
+  }
+};
