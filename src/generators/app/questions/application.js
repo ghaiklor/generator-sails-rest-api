451f307ee0ab58bf695594aebc8b0374e0dc01ee
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-16 17:36:57 +0300

    Move all ES6 questions to the generators

--- src/generators/app/questions/application.js
+++
@@ -1,6 +0,0 @@
-module.exports = [{
-  type: 'input',
-  name: 'application:name',
-  message: 'Type your application name',
-  default: 'sails-rest-api'
-}];
--- src/generators/config/questions/application.js
+++
@@ -1,8 +0,0 @@
-var crypto = require('crypto');
-
-module.exports = [{
-  type: 'input',
-  name: 'application:secret',
-  message: 'Type your secret for this application',
-  default: crypto.randomBytes(32).toString('hex')
-}];
