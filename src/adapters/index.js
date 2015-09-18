520fa1cee665de2a8b64a037713d1aa188c6044f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-18 15:41:25 +0300

    Remove unused generator.js files

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,2 +1,45 @@
-require('babel/register');
-module.exports = require('./generator');
+import { Base } from 'yeoman-generator';
+import generatorArguments from './arguments';
+import generatorOptions from './options';
+import generatorSteps from './steps';
+
+export default class ServicesGenerator extends Base {
+  constructor(...args) {
+    super(...args);
+
+    Object.keys(generatorArguments).forEach(key => this.argument(key, generatorArguments[key]));
+    Object.keys(generatorOptions).forEach(key => this.option(key, generatorOptions[key]));
+  }
+
+  get configuring() {
+    return generatorSteps.configuring;
+  }
+
+  get conflicts() {
+    return generatorSteps.conflicts;
+  }
+
+  get default() {
+    return generatorSteps.default;
+  }
+
+  get end() {
+    return generatorSteps.end;
+  }
+
+  get initializing() {
+    return generatorSteps.initializing
+  }
+
+  get install() {
+    return generatorSteps.install;
+  }
+
+  get prompting() {
+    return generatorSteps.prompting
+  }
+
+  get writing() {
+    return generatorSteps.writing;
+  }
+}
