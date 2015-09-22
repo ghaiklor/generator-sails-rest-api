91631ea73b620a11efdbf40bd934212d7426f8d3
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-22 17:59:20 +0300

    Split app questions to sections

--- src/app/questions/index.js
+++ src/app/questions/index.js
@@ -1,18 +1,4 @@
-/**
- * Exports array that contains questions for prompting.
- * The array with questions is an array of Inquirer prompt objects - https://github.com/SBoudrias/Inquirer.js#prompts-type
- *
- * @example
- * export default [{
- *   type: 'input',
- *   name: 'inputName',
- *   message: 'Message for the input'
- * }];
- */
+import application from './application';
+import services from './services';
 
-export default [{
-  type: 'input',
-  name: 'application:name',
-  message: 'Type your application name',
-  default: 'sails-rest-api'
-}];
+export default {application, services};
