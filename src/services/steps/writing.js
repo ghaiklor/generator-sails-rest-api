963ab7b8eec6c4f169621c47e0d423200686bdff
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-22 17:01:28 +0300

    Change writing step in services for comma-separated list

--- src/services/steps/writing.js
+++ src/services/steps/writing.js
@@ -4,18 +4,18 @@
  */
 
 const FILES_MAP = {
-  CipherService: ['api/services/CipherService.js', 'config/services/cipher.js', 'test/unit/services/CipherService.test.js'],
-  HashService: ['api/services/HashService.js', 'config/services/hash.js', 'test/unit/services/HashService.test.js'],
-  ImageService: ['api/services/ImageService.js', 'config/services/image.js', 'test/unit/services/ImageService.test.js'],
-  LocationService: ['api/services/LocationService.js', 'config/services/location.js', 'test/unit/services/LocationService.test.js'],
-  MailerService: ['api/services/MailerService.js', 'config/services/mailer.js', 'test/unit/services/MailerService.test.js'],
-  PaymentService: ['api/services/PaymentService.js', 'config/services/payment.js', 'test/unit/services/PaymentService.test.js'],
-  PusherService: ['api/services/PusherService.js', 'config/services/pusher.js', 'test/unit/services/PusherService.test.js'],
-  SmsService: ['api/services/SmsService.js', 'config/services/sms.js', 'test/unit/services/SmsService.test.js'],
-  SocialService: ['api/services/SocialService.js', 'config/services/social.js', 'test/unit/services/SocialService.test.js'],
-  StorageService: ['api/services/StorageService.js', 'config/services/storage.js', 'test/unit/services/StorageService.test.js']
+  cipher: ['api/services/CipherService.js', 'config/services/cipher.js', 'test/unit/services/CipherService.test.js'],
+  hash: ['api/services/HashService.js', 'config/services/hash.js', 'test/unit/services/HashService.test.js'],
+  image: ['api/services/ImageService.js', 'config/services/image.js', 'test/unit/services/ImageService.test.js'],
+  location: ['api/services/LocationService.js', 'config/services/location.js', 'test/unit/services/LocationService.test.js'],
+  mailer: ['api/services/MailerService.js', 'config/services/mailer.js', 'test/unit/services/MailerService.test.js'],
+  payment: ['api/services/PaymentService.js', 'config/services/payment.js', 'test/unit/services/PaymentService.test.js'],
+  pusher: ['api/services/PusherService.js', 'config/services/pusher.js', 'test/unit/services/PusherService.test.js'],
+  sms: ['api/services/SmsService.js', 'config/services/sms.js', 'test/unit/services/SmsService.test.js'],
+  social: ['api/services/SocialService.js', 'config/services/social.js', 'test/unit/services/SocialService.test.js'],
+  storage: ['api/services/StorageService.js', 'config/services/storage.js', 'test/unit/services/StorageService.test.js']
 };
 
 export default function () {
-  this.options['services'].split(',').forEach(service => FILES_MAP[service].forEach(path => this.copy(path, path)));
+  this.options['services'].split(',').forEach(service => FILES_MAP[service.toLowerCase()].forEach(path => this.copy(path, path)));
 };
