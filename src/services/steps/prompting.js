97b7f0bd119bb809df1a95a6cced6bdabff23483
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-22 16:42:30 +0300

    Remove prompting step from services

--- src/services/steps/prompting.js
+++ src/services/steps/prompting.js
@@ -3,14 +3,4 @@
  * Where you prompt users for options (where you'd call this.prompt()).
  */
 
-import chalk from 'chalk';
-import questions from '../questions';
-
-export default function () {
-  let done = this.async();
-
-  this.prompt(questions, answers => {
-    this.answers = Object.assign(this.answers || {}, answers, this.options);
-    done();
-  });
-};
+export default {};
