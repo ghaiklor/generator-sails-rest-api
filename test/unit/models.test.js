88bc272be05b3c30dc6bb7477cbaeb90611a9485
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 16:24:13 +0300

    Improve test for models generator

--- test/unit/models.test.js
+++ test/unit/models.test.js
@@ -3,21 +3,33 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:models', function () {
-  this.timeout(10000);
+  describe('Should properly handle copying templates', () => {
+    this.timeout(10000);
 
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../generators/models'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .on('end', done);
-  });
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../generators/models'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .on('end', done);
+    });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      'api/models/User.js',
-      'test/fixtures/FaultyUsers.json',
-      'test/fixtures/Users.json',
-      'test/unit/models/User.test.js'
-    ]);
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/models/User.js'
+      ]);
+    });
+
+    it('Should properly create fixtures files', () => {
+      assert.file([
+        'test/fixtures/FaultyUsers.json',
+        'test/fixtures/Users.json'
+      ]);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/unit/models/User.test.js'
+      ]);
+    });
   });
 });
