38e1850ff64c2cb01651db498b81b4c5ac323234
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 17:15:21 +0300

    Improve test for adapters generator

--- test/unit/adapters.test.js
+++ test/unit/adapters.test.js
@@ -3,19 +3,26 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:adapters', function () {
-  this.timeout(10000);
+  describe('Should properly handle copying templates', () => {
+    this.timeout(10000);
 
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../generators/adapters'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .on('end', done);
-  });
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../generators/adapters'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .on('end', done);
+    });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      'api/adapters',
-      'test/unit/adapters'
-    ]);
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/adapters/.gitkeep'
+      ]);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/unit/adapters'
+      ]);
+    });
   });
 });
