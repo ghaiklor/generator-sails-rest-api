79e2cf734200f0aa1f3c0eda63265a803e4dcf6f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 17:13:20 +0300

    Improve test for blueprints generator

--- test/unit/blueprints.test.js
+++ test/unit/blueprints.test.js
@@ -3,33 +3,40 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:blueprints', function () {
-  this.timeout(10000);
+  describe('Should properly handle copying templates', () => {
+    this.timeout(10000);
 
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../generators/blueprints'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .on('end', done);
-  });
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../generators/blueprints'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .on('end', done);
+    });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      'api/blueprints/add.js',
-      'api/blueprints/create.js',
-      'api/blueprints/destroy.js',
-      'api/blueprints/find.js',
-      'api/blueprints/findone.js',
-      'api/blueprints/populate.js',
-      'api/blueprints/remove.js',
-      'api/blueprints/update.js',
-      'test/unit/blueprints/add.test.js',
-      'test/unit/blueprints/create.test.js',
-      'test/unit/blueprints/destroy.test.js',
-      'test/unit/blueprints/find.test.js',
-      'test/unit/blueprints/findone.test.js',
-      'test/unit/blueprints/populate.test.js',
-      'test/unit/blueprints/remove.test.js',
-      'test/unit/blueprints/update.test.js'
-    ]);
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/blueprints/add.js',
+        'api/blueprints/create.js',
+        'api/blueprints/destroy.js',
+        'api/blueprints/find.js',
+        'api/blueprints/findone.js',
+        'api/blueprints/populate.js',
+        'api/blueprints/remove.js',
+        'api/blueprints/update.js'
+      ]);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/unit/blueprints/add.test.js',
+        'test/unit/blueprints/create.test.js',
+        'test/unit/blueprints/destroy.test.js',
+        'test/unit/blueprints/find.test.js',
+        'test/unit/blueprints/findone.test.js',
+        'test/unit/blueprints/populate.test.js',
+        'test/unit/blueprints/remove.test.js',
+        'test/unit/blueprints/update.test.js'
+      ])
+    });
   });
 });
