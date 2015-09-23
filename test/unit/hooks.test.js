914178f964fe8526efa7cdac5ace015991554449
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 16:28:49 +0300

    Improve test for hooks generator

--- test/unit/hooks.test.js
+++ test/unit/hooks.test.js
@@ -3,19 +3,26 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:hooks', function () {
-  this.timeout(10000);
+  describe('Should properly handle copying templates', () => {
+    this.timeout(10000);
 
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../generators/hooks'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .on('end', done);
-  });
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../generators/hooks'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .on('end', done);
+    });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      'api/hooks/pluralize-models.js',
-      'test/unit/hooks/pluralize-models.test.js'
-    ]);
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/hooks/pluralize-models.js'
+      ]);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/unit/hooks/pluralize-models.test.js'
+      ]);
+    });
   });
 });
