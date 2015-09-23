060b540fc285d5a1c6c059715efd0bf2ad7cd0cf
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 16:12:33 +0300

    Improve test for responses

--- test/unit/responses.test.js
+++ test/unit/responses.test.js
@@ -3,33 +3,40 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:responses', function () {
-  this.timeout(10000);
+  describe('Should properly handle copying templates', () => {
+    this.timeout(10000);
 
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../generators/responses'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .on('end', done);
-  });
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../generators/responses'))
+        .inDir(path.join(os.tmpdir(), './temp-test'))
+        .on('end', done);
+    });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      'api/responses/badRequest.js',
-      'api/responses/created.js',
-      'api/responses/forbidden.js',
-      'api/responses/negotiate.js',
-      'api/responses/notFound.js',
-      'api/responses/ok.js',
-      'api/responses/serverError.js',
-      'api/responses/unauthorized.js',
-      'test/unit/responses/badRequest.test.js',
-      'test/unit/responses/created.test.js',
-      'test/unit/responses/forbidden.test.js',
-      'test/unit/responses/negotiate.test.js',
-      'test/unit/responses/notFound.test.js',
-      'test/unit/responses/ok.test.js',
-      'test/unit/responses/serverError.test.js',
-      'test/unit/responses/unauthorized.test.js'
-    ]);
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/responses/badRequest.js',
+        'api/responses/created.js',
+        'api/responses/forbidden.js',
+        'api/responses/negotiate.js',
+        'api/responses/notFound.js',
+        'api/responses/ok.js',
+        'api/responses/serverError.js',
+        'api/responses/unauthorized.js'
+      ]);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/unit/responses/badRequest.test.js',
+        'test/unit/responses/created.test.js',
+        'test/unit/responses/forbidden.test.js',
+        'test/unit/responses/negotiate.test.js',
+        'test/unit/responses/notFound.test.js',
+        'test/unit/responses/ok.test.js',
+        'test/unit/responses/serverError.test.js',
+        'test/unit/responses/unauthorized.test.js'
+      ]);
+    });
   });
 });
