b0fe9bff4221db81f3465f61e37578fcb5b4a7d4
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-23 16:47:40 +0300

    Remove unused prompting step from config generator

--- src/config/steps/prompting.js
+++ src/config/steps/prompting.js
@@ -3,15 +3,4 @@
  * Where you prompt users for options (where you'd call this.prompt()).
  */
 
-import chalk from 'chalk';
-import questions from '../questions';
-
-export default function () {
-  let done = this.async();
-
-  this.log(chalk.yellow('\nConfiguration questions:'));
-  this.prompt(questions, answers => {
-    this.answers = Object.assign(this.answers || {}, answers);
-    done();
-  });
-};
+export default {};
