4c22c3a0b05a997be46439c501e7be1304b233f8
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-05 11:54:12 +0300

    Done with policies generator

--- src/policies/templates/api/policies/isAuthenticated.js
+++ src/policies/templates/api/policies/isAuthenticated.js
@@ -7,7 +7,7 @@ import passport from 'passport';
 
 export default function (req, res, next) {
   passport.authenticate('jwt', (error, user, info) => {
-    if (error || !user) return res.negotiate(Object.assign(error || {}, info));
+    if (error || !user) return res.negotiate(error || info);
 
     req.user = user;
 
--- src/policies/templates/test/unit/policies/isAuthenticated.test.js
+++ src/policies/templates/test/unit/policies/isAuthenticated.test.js
@@ -1,71 +1,21 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
-import isUser from '../../../api/policies/isAuthenticated';
+import isAuthenticated from '../../../api/policies/isAuthenticated';
 import { jwt } from '../../../api/services/CipherService';
 
-let token = "Bearer " + jwt.encodeSync({id: 2});
-let tokenFail = token + '3';
+const req = {headers: {authorization: `Bearer ${jwt.encodeSync({id: 2})}`}};
+const reqFail = {headers: {authorization: `Bearer 1234567890`}};
+const res = {negotiate: sinon.spy()};
 
-let req = {
-  headers: {
-    authorization: token
-  }
-};
+describe('policies:isAuthenticated', () => {
+  it('Should call next() callback', done => {
+    isAuthenticated(req, res, () => {
+      assert.notOk(res.negotiate.called);
+      assert.isObject(req.user);
 
-let res = {
-  serverError: function () {
-  },
-  unauthorized: function () {
-  }
-};
-
-describe("policies:isUser", function () {
-  it("should call next() callback", function (done) {
-    let stubUnauthorized = sinon.stub(res, 'unauthorized');
-    let stubServerError = sinon.stub(res, 'serverError');
-    let nextCb = sinon.spy();
-
-    isUser(req, res, nextCb);
-
-    let i = setInterval(function () {
-      if (!stubUnauthorized.called && !stubServerError.called && !nextCb.called)
-        return;
-
-      clearInterval(i);
-
-      assert(!stubServerError.called);
-      assert(!stubUnauthorized.called);
-      assert(nextCb.called);
-
-      res.serverError.restore();
-      res.unauthorized.restore();
+      res.negotiate.restore();
 
       done();
-    }, 20);
-  });
-
-  it("should return unauthorized error", function (done) {
-    let stubUnauthorized = sinon.stub(res, 'unauthorized');
-    let stubServerError = sinon.stub(res, 'serverError');
-    let nextCb = sinon.spy();
-
-    req.headers.authorization = tokenFail;
-    isUser(req, res, nextCb);
-
-    let i = setInterval(function () {
-      if (!stubUnauthorized.called && !stubServerError.called && !nextCb.called)
-        return;
-
-      clearInterval(i);
-
-      assert(!stubServerError.called);
-      assert(!nextCb.called);
-      assert(stubUnauthorized.called);
-
-      res.serverError.restore();
-      res.unauthorized.restore();
-
-      done();
-    }, 20);
+    });
   });
 });
