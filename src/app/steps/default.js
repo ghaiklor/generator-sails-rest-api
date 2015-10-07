9acc77eb22579e5cb85c3aa12e497a855e95f0bb
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-07 18:57:38 +0300

    Remove local requiring of generators

--- src/app/steps/default.js
+++ src/app/steps/default.js
@@ -6,20 +6,16 @@
 export default function () {
   this.composeWith('sails-rest-api:adapters', {
     options: {}
-  }, {
-    local: require.resolve('../../adapters')
   });
 
-  this.composeWith('sails-rest-api:authentication', {}, {
-    local: require.resolve('../../authentication')
+  this.composeWith('sails-rest-api:authentication', {
+    options: {}
   });
 
   this.composeWith('sails-rest-api:blueprints', {
     options: {
       'use-default': this.answers['blueprints:use-default']
     }
-  }, {
-    local: require.resolve('../../blueprints')
   });
 
   this.composeWith('sails-rest-api:config', {
@@ -33,36 +29,34 @@ export default function () {
       'dynamo-secret-access-key': this.answers['config:dynamo-secret-access-key'],
       'dynamo-region': this.answers['config:dynamo-region']
     }
-  }, {
-    local: require.resolve('../../config')
   });
 
-  this.composeWith('sails-rest-api:controllers', {}, {
-    local: require.resolve('../../controllers')
+  this.composeWith('sails-rest-api:controllers', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:cron', {}, {
-    local: require.resolve('../../cron')
+  this.composeWith('sails-rest-api:cron', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:hooks', {}, {
-    local: require.resolve('../../hooks')
+  this.composeWith('sails-rest-api:hooks', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:logger', {}, {
-    local: require.resolve('../../logger')
+  this.composeWith('sails-rest-api:logger', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:models', {}, {
-    local: require.resolve('../../models')
+  this.composeWith('sails-rest-api:models', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:policies', {}, {
-    local: require.resolve('../../policies')
+  this.composeWith('sails-rest-api:policies', {
+    options: {}
   });
 
-  this.composeWith('sails-rest-api:responses', {}, {
-    local: require.resolve('../../responses')
+  this.composeWith('sails-rest-api:responses', {
+    options: {}
   });
 
   this.composeWith('sails-rest-api:services', {
@@ -76,7 +70,5 @@ export default function () {
       'sms-provider': this.answers['services:sms:provider'],
       'storage-provider': this.answers['services:storage:provider']
     }
-  }, {
-    local: require.resolve('../../services')
   });
 };
