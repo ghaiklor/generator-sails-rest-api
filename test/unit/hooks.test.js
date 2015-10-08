68fe217691b6fcdf635c191dbcb19f843339cb1e
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-08 17:00:50 +0300

    Done with hooks generator

--- test/unit/hooks.test.js
+++ test/unit/hooks.test.js
@@ -2,15 +2,9 @@ import path from 'path';
 import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
-describe('sails-rest-api:hooks', function () {
-  describe('Should properly handle copying templates', () => {
-    this.timeout(10000);
-
-    before(done => {
-      test
-        .run(path.join(__dirname, '../../src/hooks'))
-        .on('end', done);
-    });
+describe('sails-rest-api:hooks', () => {
+  describe('Should properly handle default configuration', () => {
+    before(done => test.run(path.join(__dirname, '../../src/hooks')).on('end', done));
 
     it('Should properly create api files', () => {
       assert.file([
