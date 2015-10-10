5c95249373d0dcf8810a417db7de3dfccf08b6a6
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-10 13:13:28 +0300

    Remove hooks configuration from old parts of code

--- src/config/templates/config/env/test.js
+++ src/config/templates/config/env/test.js
@@ -10,14 +10,5 @@ export default {
   models: {
     connection: 'memory',
     migrate: 'drop'
-  },
-  hooks: {
-    csrf: false,
-    grunt: false,
-    i18n: false,
-    pubsub: false,
-    session: false,
-    sockets: false,
-    views: false
   }
 };
