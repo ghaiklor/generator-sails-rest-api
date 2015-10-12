73f2f750f2391a6c118404860a3335b36590ac26
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-12 20:01:54 +0300

    Update description in blueprints generator

--- src/blueprints/index.js
+++ src/blueprints/index.js
@@ -9,6 +9,8 @@ export default class BlueprintsGenerator extends Base {
 
     Object.keys(generatorArguments).forEach(key => this.argument(key, generatorArguments[key]));
     Object.keys(generatorOptions).forEach(key => this.option(key, generatorOptions[key]));
+
+    this.description = 'Scaffolds the optimized blueprints for REST API';
   }
 
   get configuring() {
