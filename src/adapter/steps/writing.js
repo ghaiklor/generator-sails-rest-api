a70727bd8cfe364adcdd844405dda770eef0d73a
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-13 16:49:56 +0300

    Renames Adapter.template to Adapter.js

--- src/adapter/steps/writing.js
+++ src/adapter/steps/writing.js
@@ -3,8 +3,8 @@
  * Where you write the generator specific files (routes, controllers, etc)
  */
 
-const GENERIC_ADAPTER_TEMPLATE = 'api/adapters/Adapter.template';
-const GENERIC_ADAPTER_TEST_TEMPLATE = 'test/unit/adapters/Adapter.template';
+const GENERIC_ADAPTER_TEMPLATE = 'api/adapters/Adapter.js';
+const GENERIC_ADAPTER_TEST_TEMPLATE = 'test/unit/adapters/Adapter.js';
 
 export default function () {
   let name = this['adapter-name'].charAt(0).toUpperCase() + this['adapter-name'].slice(1);
