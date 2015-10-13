a214b1edf602fe1654f91d3e994e65ff3f1d8d2c
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-13 17:29:25 +0300

    Implements replacing Adapter with empty in adapters name

--- src/adapter/steps/writing.js
+++ src/adapter/steps/writing.js
@@ -7,7 +7,7 @@ const GENERIC_ADAPTER_TEMPLATE = 'api/adapters/Adapter.js';
 const GENERIC_ADAPTER_TEST_TEMPLATE = 'test/unit/adapters/Adapter.js';
 
 export default function () {
-  let name = this['adapter-name'].charAt(0).toUpperCase() + this['adapter-name'].slice(1);
+  let name = (this['adapter-name'].charAt(0).toUpperCase() + this['adapter-name'].slice(1)).replace(/Adapter/, '');
 
   this.template(GENERIC_ADAPTER_TEMPLATE, `api/adapters/${name}Adapter.js`, {name});
   this.template(GENERIC_ADAPTER_TEST_TEMPLATE, `test/unit/adapters/${name}Adapter.test.js`, {name});
