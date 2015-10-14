ced84f283761f38156960f2d8e001ba6f3cd2b08
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-14 16:09:05 +0300

    Add description to hook generator

--- src/hook/index.js
+++ src/hook/index.js
@@ -9,6 +9,8 @@ export default class HookGenerator extends Base {
 
     Object.keys(generatorArguments).forEach(key => this.argument(key, generatorArguments[key]));
     Object.keys(generatorOptions).forEach(key => this.option(key, generatorOptions[key]));
+
+    this.description = 'Scaffold a custom hook in api/hooks';
   }
 
   get configuring() {
