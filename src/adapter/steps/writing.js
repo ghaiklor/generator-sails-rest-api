fced71d1f755aed879c126b74d30d07fb655f596
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-16 15:37:01 +0300

    Simplify adapters template folders

--- src/adapter/steps/writing.js
+++ src/adapter/steps/writing.js
@@ -3,12 +3,15 @@
  * Where you write the generator specific files (routes, controllers, etc)
  */
 
-const GENERIC_ADAPTER_TEMPLATE = 'api/adapters/Adapter.js';
-const GENERIC_ADAPTER_TEST_TEMPLATE = 'test/unit/adapters/Adapter.test.js';
+const SOURCE_ADAPTER = `Adapter.js`;
+const SOURCE_ADAPTER_TEST = `Adapter.test.js`;
+
+const DESTINATION_ADAPTER = (name) => `api/adapters/${name}Adapter.js`;
+const DESTINATION_ADAPTER_TEST = (name) => `test/unit/adapters/${name}Adapter.test.js`;
 
 export default function () {
   let name = (this['adapter-name'].charAt(0).toUpperCase() + this['adapter-name'].slice(1)).replace(/Adapter/, '');
 
-  this.template(GENERIC_ADAPTER_TEMPLATE, `api/adapters/${name}Adapter.js`, {name});
-  this.template(GENERIC_ADAPTER_TEST_TEMPLATE, `test/unit/adapters/${name}Adapter.test.js`, {name});
+  this.template(SOURCE_ADAPTER, DESTINATION_ADAPTER(name), {name});
+  this.template(SOURCE_ADAPTER_TEST, DESTINATION_ADAPTER_TEST(name), {name});
 };
