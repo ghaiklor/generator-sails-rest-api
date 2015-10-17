7823d8c81d2c7959c30f5fe646c2b90db8a90349
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-17 14:40:54 +0300

    Improve tests

--- test/unit/logger.test.js
+++ test/unit/logger.test.js
@@ -3,6 +3,22 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:logger', () => {
+  describe('Should properly scaffold default configuration', () => {
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../src/logger'))
+        .on('end', done)
+    });
+
+    it('Should properly create configuration files', () => {
+      assert.file([
+        'config/log.js'
+      ]);
+
+      assert.fileContent('config/log.js', /bunyan/);
+    });
+  });
+
   describe('Should properly scaffold bunyan configuration', () => {
     before(done => {
       test
@@ -22,7 +38,7 @@ describe('sails-rest-api:logger', () => {
     });
   });
 
-  describe('Should properly scaffold default configuration', () => {
+  describe('Should properly scaffold Sails default configuration', () => {
     before(done => {
       test
         .run(path.join(__dirname, '../../src/logger'))
