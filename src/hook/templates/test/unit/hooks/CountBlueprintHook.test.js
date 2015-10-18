87cc37f03e42c09158d58df4b16ae5ccc3e52346
IncoCode <akulich.dimon@gmail.com>
2015-10-18 22:13:18 +0300

    Add tests for CountBlueprint hook

---
+++ src/hook/templates/test/unit/hooks/CountBlueprintHook.test.js
@@ -0,0 +1,73 @@
+import { assert } from 'chai';
+import sinon from 'sinon';
+import _ from 'lodash';
+import hook from '../../../api/hooks/CountBlueprintHook';
+
+const sailsLoc = {
+  middleware: {
+    controllers: {
+      test1: {}, test2: {}, test3: {}
+    }
+  },
+  models: {
+    test1: {
+      identity: 'test1'
+    },
+    test4: {}
+  },
+  config: {
+    blueprints: {
+      pluralize: false,
+      prefix: '/v1'
+    }
+  },
+  router: {
+    bind: sinon.spy()
+  }
+};
+
+describe('hooks:countBlueprint', () => {
+  beforeEach(done => {
+    sailsLoc.router.bind.reset();
+    _.set(sailsLoc, 'config.blueprints.pluralize', false);
+    done();
+  });
+
+  it('Should properly exports', done => {
+    assert.isFunction(hook);
+    done();
+  });
+
+  it('Should add :model/count route only for RESTful models', done => {
+    var route = '/v1/test1/count';
+
+    sailsLoc.on = (event, func) => {
+      func();
+
+      assert.ok(sailsLoc.router.bind.calledOnce);
+      assert.ok(sailsLoc.router.bind.calledWith(route));
+
+      done();
+    };
+
+    hook(sailsLoc).initialize(() => {
+    });
+  });
+
+  it('Should add :model/count route only for RESTful models (pluralize enabled)', done => {
+    var route = '/v1/test1s/count';
+    _.set(sailsLoc, 'config.blueprints.pluralize', true);
+
+    sailsLoc.on = (event, func) => {
+      func();
+
+      assert.ok(sailsLoc.router.bind.calledOnce);
+      assert.ok(sailsLoc.router.bind.calledWith(route));
+
+      done();
+    };
+
+    hook(sailsLoc).initialize(() => {
+    });
+  });
+});
