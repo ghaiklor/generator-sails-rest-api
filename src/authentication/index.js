d89e433db30c8167b6c20a11e195c8697d3e56cf
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-21 17:35:20 +0300

    docs(authentication): Add description to generator

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -9,6 +9,8 @@ export default class AuthenticationGenerator extends Base {
 
     Object.keys(generatorArguments).forEach(key => this.argument(key, generatorArguments[key]));
     Object.keys(generatorOptions).forEach(key => this.option(key, generatorOptions[key]));
+
+    this.description = 'Scaffolds a new authentication layer';
   }
 
   get configuring() {
