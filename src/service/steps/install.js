bcdbdd59910c58723a6608b1fd19dce1759626f4
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-21 12:02:15 +0300

    refactor(service): Make dependencies map
    Add dependencies map for each of services in install step

--- src/service/steps/install.js
+++ src/service/steps/install.js
@@ -3,8 +3,23 @@
  * Where installation are run (npm, bower)
  */
 
+const DEPENDENCIES = {
+  cipher: ['sails-service-cipher'],
+  hash: ['sails-service-hash'],
+  image: ['sails-service-image'],
+  location: ['sails-service-location'],
+  mailer: ['sails-service-mailer'],
+  payment: ['sails-service-payment'],
+  pusher: ['sails-service-pusher'],
+  sms: ['sails-service-sms'],
+  social: ['sails-service-social'],
+  storage: ['sails-service-storage']
+};
+
 export default function () {
   let name = this['service-name'].replace(/Service/, '').toLowerCase();
 
-  this.npmInstall([`sails-service-${name}`], {save: true});
+  if (DEPENDENCIES[name]) {
+    this.npmInstall(DEPENDENCIES[name], {save: true});
+  }
 };
