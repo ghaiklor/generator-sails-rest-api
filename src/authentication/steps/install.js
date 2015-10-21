b687c6d803bd7a026d68927e5ec16a7323814c8b
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-21 18:10:19 +0300

    refactor(authentication): Move passport dependencies to hash map
    Add PASSPORT_DEPENDENCIES object that contains all passport strategies

--- src/authentication/steps/install.js
+++ src/authentication/steps/install.js
@@ -3,9 +3,28 @@
  * Where installation are run (npm, bower)
  */
 
+const PASSPORT_DEPENDENCIES = {
+  local: ['passport-local'],
+  jwt: ['passport-jwt'],
+  facebook: ['passport-facebook-token'],
+  twitter: ['passport-twitter-token'],
+  vkontakte: ['passport-vkontakte-token'],
+  foursquare: ['passport-foursquare-token'],
+  github: ['passport-github-token'],
+  instagram: ['passport-instagram-token'],
+  paypal: ['passport-paypal-token'],
+  reddit: ['passport-reddit-token'],
+  soundcloud: ['passport-soundcloud-token'],
+  windowsLive: ['passport-windows-live-token'],
+  twitch: ['passport-twitch-token'],
+  yandex: ['passport-yandex-token'],
+  amazon: ['passport-amazon-token'],
+  googlePlus: ['passport-google-plus-token'],
+  yahoo: ['passport-yahoo-token']
+};
+
 export default function () {
-  this.npmInstall(
-    this.options['social-networks'].split(',').map(service => `passport-${service.toLowerCase()}-token`),
-    {save: true}
-  )
+  let passportDependencies = Object.keys(PASSPORT_DEPENDENCIES).reduce((deps, strategy) => deps.concat(PASSPORT_DEPENDENCIES[strategy]), []);
+
+  this.npmInstall(passportDependencies, {save: true});
 };
