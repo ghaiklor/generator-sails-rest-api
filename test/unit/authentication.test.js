b2720213b693b1108ae2cd611ffa743c000a1f1e
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-21 18:19:13 +0300

    test(authentication): Write tests for custom secret-key option

--- test/unit/authentication.test.js
+++ test/unit/authentication.test.js
@@ -3,7 +3,7 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:authentication', () => {
-  describe('Should properly handle default configuration', () => {
+  describe('Should properly scaffold default authentication layer', () => {
     before(done => test.run(path.join(__dirname, '../../src/authentication')).on('end', done));
 
     it('Should properly create api files', () => {
@@ -17,8 +17,11 @@ describe('sails-rest-api:authentication', () => {
 
     it('Should properly create configuration files', () => {
       assert.file([
-        'config/passport.js'
+        'config/passport.js',
+        'config/policies.js'
       ]);
+
+      assert.fileContent('config/passport.js', /secretOrKey: 'DEFAULT_SECRET_KEY'/);
     });
 
     it('Should properly create test files', () => {
@@ -29,7 +32,47 @@ describe('sails-rest-api:authentication', () => {
         'test/unit/controllers/UserController.test.js',
         'test/unit/models/User.test.js',
         'test/unit/policies/isAuthenticated.test.js'
-      ])
-    })
+      ]);
+    });
+  });
+
+  describe('Should properly scaffold authentication layer with custom secret key', () => {
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../src/authentication'))
+        .withOptions({
+          'secret-key': '1234567890'
+        })
+        .on('end', done)
+    });
+
+    it('Should properly create api files', () => {
+      assert.file([
+        'api/controllers/AuthController.js',
+        'api/controllers/UserController.js',
+        'api/models/User.js',
+        'api/policies/isAuthenticated.js'
+      ]);
+    });
+
+    it('Should properly create configuration files', () => {
+      assert.file([
+        'config/passport.js',
+        'config/policies.js'
+      ]);
+
+      assert.fileContent('config/passport.js', /secretOrKey: '1234567890'/);
+    });
+
+    it('Should properly create test files', () => {
+      assert.file([
+        'test/fixtures/FaultyUsers.js',
+        'test/fixtures/Users.js',
+        'test/unit/controllers/AuthController.test.js',
+        'test/unit/controllers/UserController.test.js',
+        'test/unit/models/User.test.js',
+        'test/unit/policies/isAuthenticated.test.js'
+      ]);
+    });
   });
 });
