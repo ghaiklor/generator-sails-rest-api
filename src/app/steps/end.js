d2f0aba942dad78f66516394d907e04331851d75
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-22 20:00:52 +0300

    feat(app): Add message in end step
    Prints out message how to continue with developing your project

--- src/app/steps/end.js
+++ src/app/steps/end.js
@@ -3,5 +3,14 @@
  * Called last, cleanup, say good bye, etc
  */
 
+import chalk from 'chalk';
+import printMessage from 'print-message';
+
 export default function () {
+  printMessage([
+    'Enjoy your Sails REST API project!',
+    '---',
+    'Start developing with creating models',
+    chalk.blue('yo sails-rest-api:model')
+  ]);
 };
