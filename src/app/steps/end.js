e8c4785c3c341820781b5620dd16d2ad94038625
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-22 20:18:36 +0300

    style(app): Update end message

--- src/app/steps/end.js
+++ src/app/steps/end.js
@@ -8,9 +8,14 @@ import printMessage from 'print-message';
 
 export default function () {
   printMessage([
-    'Enjoy your Sails REST API project!',
-    '---',
-    'Start developing with creating models',
-    chalk.blue('yo sails-rest-api:model')
-  ]);
+    `Enjoy your ${chalk.red('Sails REST API')} project!`,
+    `---`,
+    `Next steps:`,
+    `${chalk.yellow('1)')} Create a model in your app:`,
+    chalk.blue('yo sails-rest-api:model Ticket'),
+    `${chalk.yellow('2)')} Compose your API and run:`,
+    chalk.blue('npm start')
+  ], {
+    printFn: this.log
+  });
 };
