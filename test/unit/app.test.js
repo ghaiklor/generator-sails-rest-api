cbc595dc66bc30e51d0250000532e5407f463e7a
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-23 17:38:33 +0300

    test(app): Improve test coverage

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -3,42 +3,94 @@ import os from 'os';
 import { assert, test } from 'yeoman-generator';
 
 describe('sails-rest-api:app', () => {
-  before(done => {
-    test
-      .run(path.join(__dirname, '../../src/app'))
-      .withGenerators([
-        [test.createDummyGenerator(), 'sails-rest-api:adapter'],
-        [test.createDummyGenerator(), 'sails-rest-api:authentication'],
-        [test.createDummyGenerator(), 'sails-rest-api:blueprint'],
-        [test.createDummyGenerator(), 'sails-rest-api:config'],
-        [test.createDummyGenerator(), 'sails-rest-api:controller'],
-        [test.createDummyGenerator(), 'sails-rest-api:cron'],
-        [test.createDummyGenerator(), 'sails-rest-api:hook'],
-        [test.createDummyGenerator(), 'sails-rest-api:logger'],
-        [test.createDummyGenerator(), 'sails-rest-api:model'],
-        [test.createDummyGenerator(), 'sails-rest-api:policy'],
-        [test.createDummyGenerator(), 'sails-rest-api:response'],
-        [test.createDummyGenerator(), 'sails-rest-api:service'],
-        [test.createDummyGenerator(), 'sails-rest-api:swagger']
+  describe('Should properly scaffold with default configuration', () => {
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../src/app'))
+        .withGenerators([
+          [test.createDummyGenerator(), 'sails-rest-api:adapter'],
+          [test.createDummyGenerator(), 'sails-rest-api:authentication'],
+          [test.createDummyGenerator(), 'sails-rest-api:blueprint'],
+          [test.createDummyGenerator(), 'sails-rest-api:config'],
+          [test.createDummyGenerator(), 'sails-rest-api:controller'],
+          [test.createDummyGenerator(), 'sails-rest-api:cron'],
+          [test.createDummyGenerator(), 'sails-rest-api:hook'],
+          [test.createDummyGenerator(), 'sails-rest-api:logger'],
+          [test.createDummyGenerator(), 'sails-rest-api:model'],
+          [test.createDummyGenerator(), 'sails-rest-api:policy'],
+          [test.createDummyGenerator(), 'sails-rest-api:response'],
+          [test.createDummyGenerator(), 'sails-rest-api:service'],
+          [test.createDummyGenerator(), 'sails-rest-api:swagger']
+        ])
+        .on('end', done);
+    });
+
+    it('Should properly create root files', () => {
+      assert.file([
+        '.editorconfig',
+        '.gitignore',
+        '.sailsrc',
+        'app.js',
+        'Dockerfile',
+        'package.json'
+      ]);
+    });
+
+    it('Should properly create mocha.opts and bootstrap', () => {
+      assert.file([
+        'test/bootstrap.js',
+        'test/mocha.opts'
       ])
-      .on('end', done);
+    });
   });
 
-  it('Should properly create root files', () => {
-    assert.file([
-      '.editorconfig',
-      '.gitignore',
-      '.sailsrc',
-      'app.js',
-      'Dockerfile',
-      'package.json'
-    ]);
-  });
+  describe('Should properly scaffold with custom configuration', () => {
+    before(done => {
+      test
+        .run(path.join(__dirname, '../../src/app'))
+        .withGenerators([
+          [test.createDummyGenerator(), 'sails-rest-api:adapter'],
+          [test.createDummyGenerator(), 'sails-rest-api:authentication'],
+          [test.createDummyGenerator(), 'sails-rest-api:blueprint'],
+          [test.createDummyGenerator(), 'sails-rest-api:config'],
+          [test.createDummyGenerator(), 'sails-rest-api:controller'],
+          [test.createDummyGenerator(), 'sails-rest-api:cron'],
+          [test.createDummyGenerator(), 'sails-rest-api:hook'],
+          [test.createDummyGenerator(), 'sails-rest-api:logger'],
+          [test.createDummyGenerator(), 'sails-rest-api:model'],
+          [test.createDummyGenerator(), 'sails-rest-api:policy'],
+          [test.createDummyGenerator(), 'sails-rest-api:response'],
+          [test.createDummyGenerator(), 'sails-rest-api:service'],
+          [test.createDummyGenerator(), 'sails-rest-api:swagger']
+        ])
+        .withOptions({
+          'skip-update': true
+        })
+        .withPrompts({
+          'authentication:enabled': false,
+          'blueprint:all': false,
+          'cron:enabled': false,
+          'swagger:enabled': false
+        })
+        .on('end', done);
+    });
 
-  it('Should properly create mocha.opts and bootstrap', () => {
-    assert.file([
-      'test/bootstrap.js',
-      'test/mocha.opts'
-    ])
+    it('Should properly create root files', () => {
+      assert.file([
+        '.editorconfig',
+        '.gitignore',
+        '.sailsrc',
+        'app.js',
+        'Dockerfile',
+        'package.json'
+      ]);
+    });
+
+    it('Should properly create mocha.opts and bootstrap', () => {
+      assert.file([
+        'test/bootstrap.js',
+        'test/mocha.opts'
+      ])
+    });
   });
 });
