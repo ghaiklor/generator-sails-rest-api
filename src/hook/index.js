d4a937fd2e8d79ec64a1619b7cbf6b820e5f680f
ghaiklor <ghaiklor@gmail.com>
2015-10-26 15:06:15 +0200

    docs(hook): Fixes typo in description of generator

--- src/hook/index.js
+++ src/hook/index.js
@@ -10,7 +10,7 @@ export default class HookGenerator extends Base {
     Object.keys(generatorArguments).forEach(key => this.argument(key, generatorArguments[key]));
     Object.keys(generatorOptions).forEach(key => this.option(key, generatorOptions[key]));
 
-    this.description = 'Scaffold a new hook';
+    this.description = 'Scaffolds a new hook';
   }
 
   get configuring() {
