7d459119bb75f4c75490b3b520915d92cd06395d
Travis Webb <me@traviswebb.com>
2015-12-02 02:59:51 -0500

    removed controller welcome msg

--- src/controller/steps/initializing.js
+++ src/controller/steps/initializing.js
@@ -3,8 +3,4 @@
  * Your initialization methods (checking current project state, getting configs, etc)
  */
 
-export default {
-  sayHello: function () {
-    this.log('Creating a new Controller with Test Suite')
-  }
-};
+export default { };
