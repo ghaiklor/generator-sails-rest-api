6b37f841e4a4af2e16284ecb8da0bfb7441109b7
Travis Webb <me@traviswebb.com>
2015-12-01 21:47:25 -0500

    removed extra controller templates

--- src/controller/templates/api/controllers/PingController.js
+++
@@ -1,8 +0,0 @@
-/**
- * PingController
- * @description :: Server-side logic that checks if app is alive
- */
-
-export function index(req, res) {
-  res.ok(req.allParams(), {message: 'HTTP server is working'});
-}
