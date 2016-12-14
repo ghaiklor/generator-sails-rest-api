01a1cbd915bc7b6d4ecd619456dfe6aa007e3fe9
Mike DeVita <mike@devita.co>
2016-12-13 20:23:05 -0700

    fix passport return code for jwt expired to 401.
    config/passport.js isn't the right place to do this since it doesnt seem
    to be getting called as expected. I validated this and then placed in
    policies/isAuthenticated.js

--- generators/authentication/templates/api/policies/isAuthenticated.js
+++ generators/authentication/templates/api/policies/isAuthenticated.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 module.exports = (req, res, next) => {
   passport.authenticate('jwt', (error, user, info) => {
+    if (info.name === 'TokenExpiredError') info.status = 401;
     if (error || !user) return res.negotiate(error || info);
 
     req.user = user;
