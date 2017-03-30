e6fdf9849b02ab13402140574063dfa7d4e6ef15
Travis J. Webb <me@traviswebb.com>
2017-03-30 13:46:45 -0400

    [fix] controller test assertion

--- generators/controller/templates/Controller.test.js
+++ generators/controller/templates/Controller.test.js
@@ -2,8 +2,10 @@ const assert = require('assert')
 
 describe('<%= controllerName %>Controller', () => {
   let <%= controllerName %>
+
   before(() => {
     <%= controllerName %> = global.app.controllers.<%= controllerName %>
-    assert(controllerName)
+    assert(<%= controllerName %>)
   })
 })
+
